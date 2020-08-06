19cf6c4cdaca5bf6f33160fb69304eeb1b1ef602
Josiah Roa <josiahroa18@gmail.com>
2020-08-06 13:59:04 -0600

    Day 4 MVP

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -5,11 +5,31 @@ f(a) + f(b) = f(c) - f(d)
 
 #q = set(range(1, 10))
 #q = set(range(1, 200))
-q = (1, 3, 4, 7, 12)
-
-
 def f(x):
     return x * 4 + 6
 
 # Your code here
+# Figure out how many combinations each set could potentiall hold
+# Assign a new a, b, c, d each time it has been checked
+# Find all combinations where f(a) + f(b) = f(c) - f(d) and print them out
+# Example output for each combination: f(1) + f(1) = f(12) - f(7) 
 
+def main():
+    q = (1, 3, 4, 7, 12)
+    spaces = ' ' * 5
+    sums = {}
+    diffs = {}
+    for x in q:
+        for y in q:
+            sums[(x, y)] = f(x) + f(y) 
+            diffs[(x, y)] = f(x) - f(y)
+
+    for x in sums.keys():
+        for y in diffs.keys():
+            if sums[x] == diffs[y]:
+                print(f"f({x[0]}) + f({x[1]}) = f({y[0]}) + f({y[1]})" + spaces + f"{sums[x]} = {diffs[y]}")
+
+
+
+if __name__ == '__main__':
+    main()
